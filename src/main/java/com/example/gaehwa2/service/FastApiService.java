c50fa5d39076a7a4d25ac0d45202a05b95508252
jkim3 <62086003+kimjieun0301@users.noreply.github.com>
2025-08-21 19:43:04 +0900

    Update FastApiService.java

--- src/main/java/com/example/gaehwa2/service/FastApiService.java
+++ src/main/java/com/example/gaehwa2/service/FastApiService.java
@@ -68,7 +68,7 @@ public class FastApiService {
 
         // URL에서 mp3 다운로드
         // 항상 고정 절대 URL 사용
-        String mp3Url = "http://localhost:9000/outputs/voice_final.mp3";
+        String mp3Url = "http://localhost:9000/static/voice_final.mp3";
 
 // 다운로드
         return restTemplate.getForObject(mp3Url, byte[].class);
